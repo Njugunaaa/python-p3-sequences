aafcce1b5141bfdd8896dd5504a9c5dca553d515
Njuguna <joshuangich2@gmail.com>
2025-05-09 06:21:30 +0300

    Initial Commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci=[]
+    if length<=0:
+        print(fibonacci)
+        return
+    elif length ==1:
+        print([0])
+        return
+    elif length ==2:
+        print([0,1])
+        return
+    fibonacci=[0,1]
+
+    while len(fibonacci)<length:
+        next_value=fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_value)
+
+    print(fibonacci)
